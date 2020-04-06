7a316c1b80baf87c920c72c033cc77763a95b854
Tom Lingham <tjlingham@gmail.com>
2020-04-06 22:10:19 +1000

    Actually fix the copy

--- bin/install/index.js
+++ bin/install/index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 "use strict";
 
-const cpy = require("cpy");
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 const PWD = process.cwd();
 
@@ -26,18 +25,18 @@ const PWD = process.cwd();
  */
 {
   // This is the path to the folder of files that we want to copy.
-  const includesPath = path.resolve(__dirname, "..", "..", "includes");
+  const includesPath = path.join(__dirname, "..", "..", "includes");
 
   // Get a list of all the files and copy each one, if it doesn't already exist
   // in the destination path.
   fs.readdirSync(includesPath).forEach((file) => {
     const filePath = path.join(includesPath, file);
     const destinationPath = path.join(PWD, file);
-
+  
     if (!fs.existsSync(destinationPath)) {
-      cpy(filePath, PWD);
+      fs.copy(filePath, destinationPath);
     }
-  });
+  })
 
   console.log("@toml.dev config files copied.");
   console.log("Just delete the ones you don't want!");
